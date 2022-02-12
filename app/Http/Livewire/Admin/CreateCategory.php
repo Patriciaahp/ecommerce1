6ff368854ce92a892b315307442e3c2d3a362e6b
Patriciaahp <patrihernandezperez9@gmail.com>
2022-02-12 10:38:02 +0000

    app/

---
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Brand;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class CreateCategory extends Component
+{
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
+    public $brands;
+    public function mount()
+    {
+        $this->getBrands();
+    }
+    public function getBrands()
+    {
+        $this->brands = Brand::all();
+    }
+
+    public function save()
+    {
+    }
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
+    public function render()
+    {
+        return view('livewire.admin.create-category');
+    }
+}

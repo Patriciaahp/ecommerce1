4a48da991d968813eae389b3332791a3320f13a3
Patriciaahp <patrihernandezperez9@gmail.com>
2022-02-15 10:54:08 +0000

    fin paso 173

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -9,14 +9,15 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
 
+
     public function index()
     {
-        $orders = Order::query();
+        $orders = Order::query()->where('status', '!=', 1);
         if (request('status')) {
             $orders->where('status', request('status'));
         }
         $orders = $orders->get();
-        for ($i = 1; $i <= 5; $i++) {
+        for ($i = 2; $i <= 5; $i++) {
             $ordersByStatus[$i] = Order::where('status', $i)->count();
         }
         return view('admin.orders.index', compact('orders', 'ordersByStatus'));

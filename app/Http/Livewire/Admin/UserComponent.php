efcb27b2a81483ee17f3dca6c4858e4f0a88d595
Patriciaahp <patrihernandezperez9@gmail.com>
2022-02-17 14:55:57 +0000

    fin paso 184

--- app/Http/Livewire/Admin/UserComponent.php
+++ app/Http/Livewire/Admin/UserComponent.php
@@ -4,13 +4,25 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UserComponent extends Component
 {
+    public $search;
+    use WithPagination;
+
     public function render()
     {
-        $users = User::paginate();
+        $users = User::where('name', 'LIKE', "%{$this->search}%")
+            ->orWhere('email', 'LIKE', "%{$this->search}%")
+            ->paginate();
+
         return view('livewire.admin.user-component', compact('users'))
             ->layout('layouts.admin');
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
     }
 }

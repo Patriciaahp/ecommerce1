5e07829622b2a156541831c1a88e1036046efde2
Patriciaahp <patrihernandezperez9@gmail.com>
2022-02-22 17:36:01 +0100

    tarea 13 a medio (semana 3)

--- tests/Browser/ShoppingCartTest.php
+++ tests/Browser/ShoppingCartTest.php
@@ -865,4 +865,61 @@ class ShoppingCartTest extends DuskTestCase
                 ->screenshot('hiddenForm') ;
      });
     }
+
+
+    /** @test */
+    public function completed_order_test()
+    {
+        $category = Category::factory()->create(['name' => 'Celulares y tablets',
+            'slug' => Str::slug('Celulares y tablets'),
+            'icon' => '<i class="fas fa-mobile-alt"></i>']);
+        $subcategory = Subcategory::create(
+            ['category_id' => 1,
+                'name' => 'Smartwatches',
+                'slug' => Str::slug('Smartwatches'),
+            ]);
+
+        $brand = $category->brands()->create([
+            'name' => 'marca'
+        ]);
+        $product = Product::factory()->create([
+            'name' => 'casa',
+            'slug' => Str::slug('casa'),
+            'description' => 'la casa asdd',
+            'price' => 39.99,
+            'subcategory_id' => 1,
+            'brand_id' => 1,
+            'quantity' => 3,
+            'status' => 2]);
+        $product->images()->create([
+            'url' => 'storage/enrf3.png'
+        ]);
+
+        $user =  User::factory()->create([
+            'name' => 'Paco García',
+            'email' => 'paco@test.com',
+            'password' => bcrypt('1234'),
+        ]);
+
+        $this->browse(function (Browser $browser) use ($product, $user) {
+            $browser
+                ->visit('/login')
+                ->pause(10)
+                ->value('#email', 'paco@test.com')
+                ->value('#password', '1234')
+                ->press('INICIAR SESIÓN')
+                ->visit('/products/ ' . $product->id )
+                ->press('AGREGAR AL CARRITO DE COMPRAS')
+                ->pause(2000)
+                ->visit('/shopping-cart')
+                ->assertSee($product->name)
+                ->pause(2000)
+                ->click('a.bg-red-600')
+                ->value('#name', 'Paco García')
+                ->value('#phone', '453533234')
+                    ->click('.order-2 .bg-gray-800')
+                ->pause(2000)
+                ->screenshot('completedOrder') ;
+        });
+    }
 }

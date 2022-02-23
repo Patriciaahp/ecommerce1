e4ca98660e4f2f2554534a56e3a5d092f1d8fd98
Patriciaahp <patrihernandezperez9@gmail.com>
2022-02-23 15:53:16 +0100

    tarea 1  (semana 4)

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,13 +3,16 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\Subcategory;
 use App\Models\User;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
@@ -117,11 +120,87 @@ public function shoppingCartIsStoredInDataBase_test()
     $product->images()->create([
         'url' => 'storage/aaa.png'
     ]);
+    $user = User::factory()->create([
+        'name' => 'Paco García',
+        'email' => 'paco@test.com',
+        'password' => bcrypt('1234'),
+    ]);
 
-
-    $this->assertDatabaseHas('shoppingcart',[
+    $this->actingAs($user);
+    Cart::add([
+        'id' => $product->id,
+        'name' => $product->name,
+        'qty' => 2,
+        'price' => $product->price,
+        'weight' => 550,
 
     ]);
+    Auth::logout();
+
+       $this ->assertDatabaseHas('shoppingcart',[
+       'identifier' =>$user->id,
+    ]);
+
+ $this->actingAs($user)
+ ->get('/shopping-cart')
+    ->assertSee($product->name);
 }
+
+
+
+    public function route_verification_test()
+    {
+
+        $category = Category::factory()->create(['name' => 'Celulares y tablets',
+            'slug' => Str::slug('Celulares y tablets'),
+            'icon' => '<i class="fas fa-mobile-alt"></i>']);
+        $subcategory = Subcategory::create(
+            ['category_id' => 1,
+                'name' => 'Smartwatches',
+                'slug' => Str::slug('Smartwatches'),
+            ]);
+
+        $brand = $category->brands()->create([
+            'name' => 'marca2'
+        ]);
+        $product = Product::factory()->create([
+            'name' => 'casa',
+            'slug' => Str::slug('casa'),
+            'description' => 'la casa asdd',
+            'price' => 39.99,
+            'subcategory_id' => 1,
+            'brand_id' => 1,
+            'quantity' => 2,
+            'status' => 2]);
+        $product->images()->create([
+            'url' => 'storage/aaa.png'
+        ]);
+        $user = User::factory()->create([
+            'name' => 'Paco García',
+            'email' => 'paco@test.com',
+            'password' => bcrypt('1234'),
+        ]);
+
+        $this->actingAs($user)
+        ->get('/login')
+            ->assertRedirect(route('/dashboard'));
+        Cart::add([
+            'id' => $product->id,
+            'name' => $product->name,
+            'qty' => 2,
+            'price' => $product->price,
+            'weight' => 550,
+
+        ]);
+        $this->get('/orders/create')
+            ->assertRedirect(route('/orders/1/payment'));
+
+        Auth::logout();
+        $this->get('/orders/create')
+      ->assertRedirect(route('/login'));
+
+
+    }
+
 
 }

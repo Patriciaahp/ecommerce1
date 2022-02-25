7fd137e5569f7edeb7f9305093c600853f12cc34
Patriciaahp <patrihernandezperez9@gmail.com>
2022-02-25 17:55:33 +0100

    tarea 6 (sin completar) (semana 4)

--- tests/Feature/OrdersTest.php
+++ tests/Feature/OrdersTest.php
@@ -171,4 +171,84 @@ public function stock_updates_in_db_making_an_order_test(){
     }
 
 
+    /** @test  */
+    public function date_from_orders_test(){
+
+        $category = Category::factory()->create(['name' => 'Celulares y tablets',
+            'slug' => Str::slug('Celulares y tablets'),
+            'icon' => '<i class="fas fa-mobile-alt"></i>']);
+        $subcategory = Subcategory::create(
+            ['category_id' => 1,
+                'name' => 'Smartwatches',
+                'slug' => Str::slug('Smartwatches'),
+            ]);
+
+        $brand = $category->brands()->create([
+            'name' => 'marca2'
+        ]);
+        $product = Product::factory()->create([
+            'name' => 'casa',
+            'slug' => Str::slug('casa'),
+            'description' => 'la casa asdd',
+            'price' => 39.99,
+            'subcategory_id' => 1,
+            'brand_id' => 1,
+            'quantity' => 2,
+            'status' => 2]);
+        $product->images()->create([
+            'url' => 'storage/aaa.png'
+        ]);
+        $user = User::factory()->create([
+            'name' => 'Paco García',
+            'email' => 'paco@test.com',
+            'password' => bcrypt('1234'),
+        ]);
+
+        $this->actingAs($user)
+            ->get('/products' . $product->id)
+            ->assertsee($product->quantity);
+        Cart::add([
+            'id' => $product->id,
+            'name' => $product->name,
+            'qty' => 1,
+            'price' => $product->price,
+            'weight' => 550,
+
+        ]);
+
+        $order = new Order();
+        $order->user_id = $user->id;
+        $order->contact = 'Paco García';
+        $order->phone = 123444;
+        $order->envio_type = 2;
+        $order->shipping_cost = 0;
+        $order->total =  2;
+        $order->status = 2;
+        $order->created_at = '01/01/1990';
+
+        $order->content = Cart::content();
+
+
+
+        $order->save();
+
+        foreach (Cart::content() as $item) {
+            discount($item);
+        }
+        $this->assertDatabaseHas('products', [
+            'quantity' => $product->quantity -1
+        ]);
+
+        $this->get('/orders')
+            ->assertSee($product->price)
+            ->assertSee('Pedidos recientes')
+        ;
+
+
+
+    }
+
+
+
+
 }
